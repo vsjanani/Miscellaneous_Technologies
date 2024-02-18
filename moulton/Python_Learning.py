d0414af2a70e7ea63f9ae4a96874358ef4f1b2c3
vsjanani <jansree90@gmail.com>
2024-02-18 13:16:35 -0800

    Completed Latest

--- moulton/Python_Learning.py
+++ moulton/Python_Learning.py
@@ -1,9 +1,4 @@
-from openpyxl import load_workbook
+x = "moosypoosypickly"
+y = "picklypoosymoosy"
 
-
-wb = load_workbook(filename="C://Users//jansr//Downloads//Miscellaneous_Technologies//moulton//see.xlsx", keep_vba='false')
-xlWorkSheet = wb.active
-strPat = xlWorkSheet['F2']
-print(strPat)
-wb.save("C://Users//jansr//Downloads//Miscellaneous_Technologies//moulton//see.xlsx")
-
+print (x+y)
