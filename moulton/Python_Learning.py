37f4120362900220fc04cc82ef2c08ebc0d1655a
vsjanani <jansree90@gmail.com>
2024-09-02 17:44:48 -0700

    learning

--- moulton/Python_Learning.py
+++ moulton/Python_Learning.py
@@ -1,4 +1,6 @@
-x = "moosypoosypickly"
-y = "picklypoosymoosy"
-
-print (x+y)
+import sys
+import numpy as np
+print(sys.path)
+arr = np.array([1,2,3])
+print(arr)
+print(list(range(1,10,2)))
---
+++ mysql_demo/SQLQuery1.sql
@@ -0,0 +1,13 @@
+create database Sql_Server;
+use Sql_Server;
+create table TestTable
+(PatientID varchar(255)
+,PatientName varchar(255)
+,PatientState varchar(255)
+,Gender varchar(255)
+,Visits int
+,Charges int)
+
+
+
+
